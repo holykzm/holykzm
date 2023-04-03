5ffbccc3567b851c09c3c46b5f4d211234726c5a
kazuma-horiike <iam@holykzm.work>
2023-04-04 01:47:23 +0900

    ✨ Write my profile

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Kazuma Horiike";
+export const SITE_DESCRIPTION =
+  "Hi, I'm Japanese Web designer & Engineer day to day studying English, Typescript and cat language 🐈🥹";
